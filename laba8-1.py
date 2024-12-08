4267ec8f2e5549002bad71ee1cb8267e141157fb
-kotiknR <kotassasinovic@gmail.com>
2024-12-08 20:29:17 +0300

    добавил файл для первого задания

---
+++ laba8-1.py
@@ -0,0 +1,18 @@
+def PowerA3(a,b):
+    b=a**3
+    return b
+b=0
+for _ in range(5):
+    while True:
+        a=input("Введите число: ")
+        check=a.replace('.','')
+        if len(a)==1:
+            if a.isdigit():
+                a=float(a)
+                break
+        else:
+            if check[1:].isdigit() and a.count('.')<=1:
+                a=float(a)
+                break
+
+    print("Результат:",PowerA3(a,b),sep=" ")
